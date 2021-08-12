35850eb51cb50766c5f696b0c4b04a742082f265
Jainy Gomes <andreajgsouza@hotmail.com>
2021-08-12 11:57:34 -0300

    adição do componente menu social

--- src/componentes/MenuSocial/estilo.js
+++ src/componentes/MenuSocial/estilo.js
@@ -1,7 +1,22 @@
 import styled from "styled-components";
 export const ConteinerMenuSocial = styled.nav`
-background-color: #007F50;
+    background-color: #007F50;
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    width: 2.5vw;
+    height: 50vh;
+    blend: PassThrough;
+    opacity: 0.7;
+    border: 1px solid ##007F50;
+    border-radius: 0 15px 15px 0;
+    padding: 5vh 0;
+    justify-content: space-around;
+    top: 20vh;
 `
 export const ItemMenuSocial = styled.a`
-
+    width: 2vw;
+    margin-right: auto;
+    margin-left: auto;
+    paddi
 `;
