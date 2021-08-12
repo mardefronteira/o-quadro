0f031ccae7d8e8c2fb9e465fa08b933ebb5e3261
Jainy Gomes <andreajgsouza@hotmail.com>
2021-08-12 10:26:42 -0300

    adição de icones

---
+++ src/componentes/MenuSocial/estilo.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const ConteinerMenuSocial = styled.nav`
+background-color: #007F50;
+`
+export const ItemMenuSocial = styled.a`
+
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import MenuSocial from "./componentes/MenuSocial"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MenuSocial />
   </React.StrictMode>,
   document.getElementById("root")
 );
