c86c59ba08073ef2af7f217231ea331a962020fd
Jainy Gomes <andreajgsouza@hotmail.com>
2021-09-17 11:02:13 -0300

    página publicação selecionada corrigida

--- src/paginas/PublicacaoSelecionada/index.js
+++ src/paginas/PublicacaoSelecionada/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 // importar estilos
 import {
-  DivFlex, TituloDestaque, H3Destaque, ImgDestaqueDireita,
+  DivFlex,
 } from '../../componentes/SecaoInformacoes/estilo';
+import {
+  DivFlexPublicacao, ImgDestaqueDireita, TituloLivro, TituloVermelho, NomeAutor,
+} from './estilo';
 // importar componentes
 import Base from '../../componentes/Base';
 import Carrossel from '../../componentes/Carrossel';
@@ -14,27 +17,27 @@ function PublicacaoSelecionada() {
       menuSocialVermelho
       estadoMenu={2}
     >
-      <DivFlex id="conteiner-principal" eColuna style={{ alignItems: 'center', gap: '2rem' }}>
-        <DivFlex id="secao-cabecalho" eColuna style={{ alignItems: 'center' }}>
-          <TituloDestaque eVermelho>CONTOS MORAIS E O CINEMA DE ÉRIC ROHMER </TituloDestaque>
-          <H3Destaque eVermelho={false}>DE ALEXANDRE GARCIA</H3Destaque>
+      <DivFlexPublicacao id="conteiner-principal" eColuna>
+        <DivFlex id="secao-cabecalho" eColuna>
+          <TituloLivro eVermelho>CONTOS MORAIS E O CINEMA DE ÉRIC ROHMER </TituloLivro>
+          <NomeAutor eVermelho={false}>DE ALEXANDRE GARCIA</NomeAutor>
         </DivFlex>
-        <DivFlex id="conteiner-info" eColuna={false} style={{ gap: '2rem' }}>
+        <DivFlex id="conteiner-info" eColuna={false}>
           <DivFlex id="secao-info" eColuna>
             <DivFlex id="secao-titulo" eColuna>
-              <H3Destaque eVermelho>SINOPSE</H3Destaque>
+              <TituloVermelho eVermelho>SINOPSE</TituloVermelho>
               {/* eslint-disable-next-line max-len */}
               <p>Alexandre Garcia segue o estilo de Rohmer na precisão seca do corte. Testemunha a matéria-prima do cineasta dos afetos e das sensações, dos gestos no mundo, em seu modo prosaico de fazer cena e reivindicar moral. Para Rohmer, vale mais o suspiro que a ilação, o inefável que o postulado – e assim atravessa a forma dura do mundo. As grandes emoções não se deixam levar, nem as grandes frases empoladas – os momentos cruciais não têm circunstância. O traço do moralismo como lição de afetos transpira com fôlego de juventude nos Contos morais, em toda intensidade e delicadeza. Garcia sabe captar este movimento e acerta na mosca do olhar rohmeriano. Pois Rohmer é o poeta do prosaico e das pequenas emoções que se expressam em micro movimentos, das superfícies do mundo, dos traços sutis na expressão, das paisagens e cenografias delicadamente construídas. - Fernão Pessoa Ramos</p>
             </DivFlex>
-            <DivFlex eColuna>
-              <H3Destaque eVermelho>SOBRE O AUTOR</H3Destaque>
+            <DivFlex id="artigo" eColuna>
+              <TituloVermelho eVermelho>SOBRE O AUTOR</TituloVermelho>
               <p>
                 {/* eslint-disable-next-line max-len */}
                 Nascido em 1985, em Curitiba, é pesquisador, realizador e professor de cinema. Trabalha na Universidade Estadual do Paraná (Unespar) e no Colégio Medianeira. É doutorando em História na UFPR, mestre em Multimeios no Instituto de Artes da Unicamp e graduado em Cinema pela Faculdade de Artes do Paraná (Unespar). Fundou e foi sócio da produtora O Quadro de 2010 a 2015.
               </p>
             </DivFlex>
-            <DivFlex eColuna>
-              <H3Destaque eVermelho>FICHA TÉCNICA</H3Destaque>
+            <DivFlex id="ficha-tecnica" eColuna>
+              <TituloVermelho eVermelho>FICHA TÉCNICA</TituloVermelho>
               <span>Editora ‏ : ‎ Arte & Letra </span>
               <span>1ª edição (21 março 2019)</span>
               <span>Idioma ‏ : ‎ Português</span>
@@ -44,10 +47,10 @@ function PublicacaoSelecionada() {
               <span>Dimensões ‏ : ‎ 16 x 1 x 23 cm</span>
             </DivFlex>
           </DivFlex>
-          <ImgDestaqueDireita src="https://placekitten.com//900/500" />
+          <ImgDestaqueDireita src="https://placekitten.com//500/700" />
         </DivFlex>
 
-      </DivFlex>
+      </DivFlexPublicacao>
 
       <Carrossel categoria={dataBase.publicacoes} taVermelho />
     </Base>
