bedb38812f7aa2e1c5cbdb44f2680784f1b2b6d7
Jainy Gomes <andreajgsouza@hotmail.com>
2021-10-08 10:17:56 -0300

    Ajustes de fonte

--- src/paginas/QuemSomos/estilo.js
+++ src/paginas/QuemSomos/estilo.js
@@ -36,7 +36,6 @@ display: flex;
 flex-direction: column;
 align-items: center;
 width: 13vw;
-
 img{
     width: 100%;
 }
@@ -47,7 +46,12 @@ text-align: center;
 color: #009F66;
 `;
 
+export const FotoPerfil = styled.img`
+padding: 1vh;
+`;
+
 export const Info = styled.article`
 text-align: left;
+line-height: 1.15em;
 
 `;
