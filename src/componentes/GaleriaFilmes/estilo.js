cd10e3de6b818d374c3d9797d549fcaddcdf281a
Jainy Gomes <andreajgsouza@hotmail.com>
2021-10-08 11:14:22 -0300

    Ajuste tamanho cards

--- src/componentes/GaleriaFilmes/estilo.js
+++ src/componentes/GaleriaFilmes/estilo.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Galeria = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, auto));
+    grid-template-columns: repeat(auto-fill, minmax(200px, 350px));
     justify-items: space-around;
     align-items: center;
     grid-gap: 5% 5%;
