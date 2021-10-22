7e1f629ab77ec0955b82915d56a1aed03344669b
mardefronteira <mmv341@nyu.edu>
2021-10-22 10:08:39 -0300

    GaleriaFilmes renderiza posters no mobile

--- src/componentes/GaleriaFilmes/Card/index.js
+++ src/componentes/GaleriaFilmes/Card/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import {
   FundoCard, Legenda, Info, ItemInfo,
 } from './estilo';
 
 function Card({ filme }) {
+  const eMovel = useMediaQuery({ query: '(max-width: 1224px)' });
   return (
     <FundoCard as={Link} to={`/filme/${filme.url}`}>
-      <img src={filme.imgIcone.src} alt={filme.imgIcone.desc} />
+      <img
+        src={eMovel ? filme.imgPoster.src : filme.imgPoster.src}
+        alt={eMovel ? filme.imgIcone.desc : filme.imgIcone.desc}
+      />
       <Legenda>
         <Info>
           <ItemInfo>{filme.titulo}</ItemInfo>
